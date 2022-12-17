feeed85e3142c81611b32e42d42ba39e27ddc4c0
sagar <sagar.catchmedia.com>
2022-12-17 20:50:37 +0530

    twak to added

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,7 +11,8 @@
     </div>
   </div>
 </template>
-
+<!--Start of Tawk.to Script-->
+<!--End of Tawk.to Script-->
 <script>
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
@@ -26,6 +27,14 @@ export default {
   async mounted() {
     console.log("fetch", window.location.origin);
     this.$store.commit("updateBaseUrl", { url: window.location.origin });
+
+    // init twak to
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/639ddd9fb0d6371309d4e762/1gkgajejp';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
   },
 };
 </script>
